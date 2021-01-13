f54571d25bde8d9c0e81aeb739c80fa5047785fa
Ang Yun Zane <zaneang.19@ichat.sp.edu.sg>
2021-01-13 16:02:04 +0800

    added gps

--- app/src/main/java/sg/LIZ/assignment1/view/activity/AddTaskActivity.java
+++ app/src/main/java/sg/LIZ/assignment1/view/activity/AddTaskActivity.java
@@ -7,22 +7,33 @@ import androidx.appcompat.widget.SwitchCompat;
 import sg.LIZ.assignment1.R;
 import sg.LIZ.assignment1.model.utilityBean.TaskDb;
 import sg.LIZ.assignment1.model.valueBean.Task;
-
+import sg.LIZ.assignment1.model.LocationTracker;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.GridLayout;
 
+import java.io.IOException;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.Locale;
 
 public class AddTaskActivity extends AppCompatActivity {
     public static final String DAY = "day";
@@ -30,10 +41,12 @@ public class AddTaskActivity extends AppCompatActivity {
     public static final String MONTH = "month";
     public static final String IS_EDIT = "isEdit";
     private GridLayout timeSelect;
-    private TimePickerDialog startTimePicker =null;
-    private TimePickerDialog endTimePicker=null;
+    private TimePickerDialog startTimePicker = null;
+    private TimePickerDialog endTimePicker = null;
     private Button buttonStartTime;
     private Button buttonEndTimeBtn;
+    private ImageButton buttonGPS;
+    private LocationTracker gps = null;
     private EditText editTextTitleInput;
     private EditText editTextDescriptionInput;
     private EditText editTextVenueInput;
@@ -67,10 +80,11 @@ public class AddTaskActivity extends AppCompatActivity {
         editTextTitleInput = findViewById(R.id.add_title);
         editTextDescriptionInput = findViewById(R.id.add_description);
         editTextVenueInput = findViewById(R.id.add_value);
+        buttonGPS = findViewById(R.id.gps_btn);
         GregorianCalendar currentDate = new GregorianCalendar();
         startHours = currentDate.get(Calendar.HOUR_OF_DAY);
         startMinutes = currentDate.get(Calendar.MINUTE);
-        final  String FORMAT = "%02d";
+        final String FORMAT = "%02d";
         buttonStartTime.setText(new StringBuilder(8)
                 .append(currentDate.get(Calendar.HOUR))
                 .append(':').append(String.format(FORMAT, startMinutes))
@@ -87,6 +101,65 @@ public class AddTaskActivity extends AppCompatActivity {
                 .append(currentDate.get(Calendar.AM_PM) == 1 ? new char[]{'P', 'M'} : new char[]{'A', 'M'}));
         //selectedDay + " " + getResources().getStringArray(R.array.month)[selectedMonth]
         ((TextView) findViewById(R.id.task_date)).setText(new StringBuilder().append(selectedDay).append(' ').append(getResources().getStringArray(R.array.month)[selectedMonth]));
+        buttonGPS.setOnClickListener(v -> {
+            if (gps == null) {
+                gps = new LocationTracker(this);
+            } else {
+                gps.getLocation();
+            }
+            if (gps.canGetLocation()) {
+                Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+                try {
+
+                    List<Address> addresses = geocoder.getFromLocation(gps.getLatitude(), gps.getLongitude(), 1);
+                    int size = addresses.size();
+                    Log.i("aaa", addresses.toString());
+                    switch (size) {
+                        case 1:
+                            editTextVenueInput.setText(addresses.get(0).getAddressLine(0));
+                            break;
+                        case 0:
+                            editTextVenueInput.setText(new StringBuilder(Double.toString(gps.getLatitude())).append(',').append(gps.getLongitude()));
+                            break;
+                        default:
+                            String[] addressLines = new String[size];
+                            for (int j = 0; j < size; ++j) {
+                                addressLines[j] = addresses.get(j).getAddressLine(0);
+                            }
+                            new AlertDialog.Builder(this)
+                                    .setTitle(R.string.select_address).setItems(addressLines, (dialog, which) -> {
+                                        editTextVenueInput.setText(addressLines[which]);
+                                    }).show();
+
+                    }
+                    buttonGPS.setImageDrawable(getDrawable(R.drawable.ic_baseline_gps_fixed_24));
+                    editTextVenueInput.addTextChangedListener(new TextWatcher(){
+
+                        @Override
+                        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+                        }
+
+                        @Override
+                        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                        }
+
+                        @Override
+                        public void afterTextChanged(Editable s) {
+                            buttonGPS.setImageDrawable(getDrawable(R.drawable.ic_baseline_gps_not_fixed_24));
+                            editTextVenueInput.removeTextChangedListener(this);
+                        }
+                    });
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(this, R.string.no_gps, Toast.LENGTH_SHORT);
+            }
+        });
     }
 
     @Override
@@ -108,22 +181,22 @@ public class AddTaskActivity extends AppCompatActivity {
     }
 
     public void onAllDayToggled(View v) {
-        timeSelect.setVisibility((allDay = ((SwitchCompat) v).isChecked())?View.INVISIBLE:View.VISIBLE);
+        timeSelect.setVisibility((allDay = ((SwitchCompat) v).isChecked()) ? View.INVISIBLE : View.VISIBLE);
     }
 
     @SuppressLint("DefaultLocale")
     public void onSetStartTime(View v) {
         /*only create the TimePickerDialog when needed*/
-        if(startTimePicker==null){
-            startTimePicker=  new TimePickerDialog(this, (v2, hours, minute) -> {
+        if (startTimePicker == null) {
+            startTimePicker = new TimePickerDialog(this, (v2, hours, minute) -> {
                 if (hours < endHours || minute < endMinutes) {
                     startHours = hours;
                     startMinutes = minute;
                     buttonStartTime.setText(new StringBuilder(8)
-                            .append( hours > 12 ? startHours - 12 : startHours)
+                            .append(hours > 12 ? startHours - 12 : startHours)
                             .append(':').append(String.format("%02d", startMinutes))
                             .append(' ')
-                            .append( hours > 11 ? new char[]{'P', 'M'} : new char[]{'A', 'M'}));
+                            .append(hours > 11 ? new char[]{'P', 'M'} : new char[]{'A', 'M'}));
                 } else {
                     Toast.makeText(this, R.string.set_start_time_err, Toast.LENGTH_LONG).show();
                 }
@@ -136,16 +209,16 @@ public class AddTaskActivity extends AppCompatActivity {
     @SuppressLint("DefaultLocale")
     public void onSetEndTime(View v) {
         /*only create the TimePickerDialog when needed*/
-        if(endTimePicker==null){
-            endTimePicker=  new TimePickerDialog(this, (v2, hours, minute) -> {
+        if (endTimePicker == null) {
+            endTimePicker = new TimePickerDialog(this, (v2, hours, minute) -> {
                 if (hours > startHours || minute > startMinutes) {
                     endHours = hours;
                     endMinutes = minute;
                     buttonEndTimeBtn.setText(new StringBuilder(8)
-                            .append(hours > 12? endHours - 12 : endHours)
+                            .append(hours > 12 ? endHours - 12 : endHours)
                             .append(':').append(String.format("%02d", endMinutes))
                             .append(' ')
-                            .append(hours > 11? new char[]{'P', 'M'} : new char[]{'A', 'M'}));
+                            .append(hours > 11 ? new char[]{'P', 'M'} : new char[]{'A', 'M'}));
                 } else {
                     Toast.makeText(this, R.string.set_end_time_err, Toast.LENGTH_LONG).show();
                 }
@@ -159,7 +232,7 @@ public class AddTaskActivity extends AppCompatActivity {
         String description = editTextDescriptionInput.getText().toString().trim();
         String venue = editTextVenueInput.getText().toString().trim();
         if (title.equals("") || description.equals("") || venue.equals("")) {
-            Toast.makeText(this,R.string.no_input_error, Toast.LENGTH_LONG).show();
+            Toast.makeText(this, R.string.no_input_error, Toast.LENGTH_LONG).show();
         } else {
             if (allDay) {
                 startMinutes = -1;
