f9eeb060320dce61dd8a3b8b69218343b546717b
Lucas <lucastan03.19@ichat.sp.edu.sg>
2021-02-02 17:30:59 +0800

    Dark and Light mode and styling

--- app/src/main/java/sg/LIZ/assignment1/view/fragment/SettingsFragment.java
+++ app/src/main/java/sg/LIZ/assignment1/view/fragment/SettingsFragment.java
@@ -4,18 +4,53 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.app.AppCompatDelegate;
+import android.content.SharedPreferences;
 
 import androidx.fragment.app.Fragment;
-
 import sg.LIZ.assignment1.R;
-import sg.LIZ.assignment1.view.layout.onSetMonth;
+
 
 public class SettingsFragment extends Fragment {
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        SharedPreferences appSettingPrefs;
 
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
+        appSettingPrefs = getActivity().getSharedPreferences("AppSettingPrefs", 0);
+        //sharedPrefsEdit = SharedPreferences.Editor = appSettingPrefs.edit();
+        boolean isNightModeOn = appSettingPrefs.getBoolean("Night Mode", false );
+
+//        if(isNightModeOn) {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+//        } else {
+//            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+//        }
+
+       view.findViewById(R.id.switchButton).setOnClickListener(new View.OnClickListener()  {
+           @Override
+           public void onClick(View view) {
+               if (isNightModeOn) { //if not night mode, set night mode, vice versa
+                   AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                   //sharedPrefsEdit("Night Mode", false);
+               } else {
+                   AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                   //setDefaultNightMode() automatically recreates any started activities.
+               }
+           }
+
+       });
+
+        view.findViewById(R.id.followSystemMode).setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View view) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+            }
+
+        });
+
+
         return view;
-    }
-}
+    } // end of onCreate
+} //end of SettingsFragment
--- app/src/main/java/sg/LIZ/assignment1/view/fragment/ViewByMonthFragment.java
+++ app/src/main/java/sg/LIZ/assignment1/view/fragment/ViewByMonthFragment.java
@@ -126,7 +126,7 @@ public class ViewByMonthFragment extends Fragment implements onSetMonth {
         selectedStyle = resources.getDrawable(R.drawable.layout_selected_day, null);
         selectedWithTaskStyle = resources.getDrawable(R.drawable.layout_selected_with_task, null);
         dayWithTaskStyle = resources.getDrawable(R.drawable.layout_day_with_task, null);
-        dayTextColour= resources.getColor(R.color.white, null);
+        dayTextColour= resources.getColor(R.color.textColour, null);
         textViewNoTaskMassage = view.findViewById(R.id.no_task_msg);
         listTaskItem = view.findViewById(R.id.list_task_item);
         listTaskItem.setLayoutManager(new LinearLayoutManager(activity, RecyclerView.VERTICAL, false));
@@ -206,7 +206,7 @@ public class ViewByMonthFragment extends Fragment implements onSetMonth {
     public void onSetYear(final CharSequence[] months) {
         if (mDatePickerDialog == null) {
             /*create the dialog object if is not already created*/
-            mDatePickerDialog = new MonthYearPickerDialog(getParentFragmentManager(), months, ((view, year, month, dayOfMonth) -> {
+            mDatePickerDialog = new MonthYearPickerDialog(getFragmentManager(), months, ((view, year, month, dayOfMonth) -> {
                 if (selectedYear != year || selectedMonth != month) {
                     selectedYear = year;
                     selectedMonth = month;
